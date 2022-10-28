39dfa524c0c013b2ad187fc6c9d377b86d24c2d3
Kennyson Le <kennysonle5.0@gmail.com>
2022-10-28 10:31:16 -0500

    Created Constants and Added PID for Motors

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+
+public final class Constants {
+    
+
+    // IDs for wheels on robot
+    public final static int frontLeftWheel = 1;
+    public final static int backLeftWheel = 2;
+    public final static int frontRightWheel = 3;
+    public final static int frontBackWheel = 4;
+
+}
