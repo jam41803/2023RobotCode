f013429d410565597a69e9009c92c49b6a51e19e
Kennyson Le <kennysonle5.0@gmail.com>
2022-11-11 16:04:09 -0600

    add Velocity to SmartdDshboard

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -7,21 +7,23 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.Joystick;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import edu.wpi.first.wpilibj.Joystick;
+
 import frc.robot.Constants;
 
 public class DriveSubsystem extends SubsystemBase {
   private final Joystick driverJoystick;
   private final DifferentialDrive differentialDrive;
   private final CANSparkMax[] motors;
+  private double[] velocity;
+
 
   public DriveSubsystem(Joystick joystick) {
     driverJoystick = joystick;
     motors = new CANSparkMax[4];
-
     //Controllers
     motors[Constants.frontLeftIndex] =
   new CANSparkMax(Constants.frontLeftMotor, MotorType.kBrushless);
@@ -31,40 +33,45 @@ public class DriveSubsystem extends SubsystemBase {
   new CANSparkMax(Constants.frontRightMotor, MotorType.kBrushless);
     motors[Constants.backRightIndex] =
   new CANSparkMax(Constants.backRightMotor, MotorType.kBrushless);
+
     //Making them mirror each other
     motors[Constants.backLeftIndex].follow(
     motors[Constants.frontLeftIndex]);
     motors[Constants.backRightIndex].follow(
     motors[Constants.frontRightIndex]);
+
     //set limits on motors
     for (CANSparkMax canSparkMax : motors) {
       canSparkMax.setOpenLoopRampRate(Constants.openLoopRampRate);
       canSparkMax.setSmartCurrentLimit(Constants.smartCurrentLimit);
     }
+
     //Inverting Motors
     motors[Constants.frontRightIndex].setInverted(true);
 
 
 
-  differentialDrive = new DifferentialDrive(
+  differentialDrive = new DifferentialDrive( //Differential Drive
     motors[Constants.frontLeftIndex],
     motors[Constants.frontRightIndex]);
   }
 
-  public void arcadeDrive() {
+  public void arcadeDrive() { // Arcade Drive
     differentialDrive.arcadeDrive(
-        driverJoystick.getRawAxis(Constants.leftJoystickY) * -1,
+        driverJoystick.getRawAxis(Constants.leftJoystickY) * -1, //Y axis was inverted
         driverJoystick.getRawAxis(Constants.rightJoystickX));
   }
 
   @Override
   public void periodic() {
-    double[] rpm = new double[4];
     int i = 0;
     for(CANSparkMax canSparkMax : motors) {
-       rpm[i++] = canSparkMax.getEncoder().getVelocity();
+       velocity[i++] = canSparkMax.getEncoder().getVelocity();
     }
-    SmartDashboard.putNumberArray("Motor RPM", rpm);
+  }
+
+  public void initSmartBoard() {
+    SmartDashboard.putNumberArray("Motor RPM", velocity);
   }
 
   @Override
